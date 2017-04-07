9412f96d90270f200b2f7ba00471acefb4b68cef
MinhTranQuang258 <finalleo258@gmail.com>
2017-04-07 10:05:15 +0700

    Action bar
    Đã thêm Actionbar + fragment nhưng chưa hoàn chỉnh

--- Bai_Tap_Lon/MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ Bai_Tap_Lon/MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,21 +1,80 @@
 package com.example.user.myapplication;
 
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 import br.com.bloder.magic.view.MagicButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.su_kien);
+        setContentView(R.layout.action_bar_bottom);
+        bottomNavigationView= (BottomNavigationView) findViewById(R.id.bottom_navi);
 
+        setFragment(0);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId()== R.id.Ghi_chep)
+                {
+                    setFragment(0);
+                }
+                else
+                {
+                    setFragment(1);
+                }
+
+                return false;
+            }
+        });
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void setFragment(int position) {
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+        switch (position) {
+            case 0:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                ChiPhi chiPhi = new ChiPhi();
+                fragmentTransaction.replace(R.id.bottom_navi, chiPhi);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                ChiTien chiTien = new ChiTien();
+                fragmentTransaction.replace(R.id.bottom_navi, chiTien);
+                fragmentTransaction.commit();
+                break;
+        }
+    }
 }
